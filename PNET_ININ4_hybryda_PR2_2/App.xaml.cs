3df0041677cc8acb1b726f5206407d7b8087a322
PMBytner <pawel.m.bytner@gmail.com>
2024-04-06 10:03:08 +0200

    Dodaj pliki projektów.

---
+++ PNET_ININ4_hybryda_PR2_2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PNET_ININ4_hybryda_PR2_2;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
